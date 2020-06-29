2e985f24906b9a64c1111289cd56251217568cd9
Fidel Montesino <fidel.montesino@gmail.com>
2020-06-29 18:27:38 +0100

    Add unit test for SettingsManager

---
+++ feature-proxy-manager/src/test/java/com/kinandcarta/create/proxytoggle/manager/settings/SettingsManagerTest.kt
@@ -0,0 +1,47 @@
+package com.kinandcarta.create.proxytoggle.manager.settings
+
+import android.content.Context
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.kinandcarta.create.proxytoggle.manager.model.Proxy
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.P])
+class SettingsManagerTest {
+
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    private lateinit var subject: SettingsManager
+
+    @Before
+    fun setUp() {
+        subject = SettingsManager(context)
+    }
+
+    @Test
+    fun `initial state is disabled`() {
+        assertThat(subject.proxySetting.value).isEqualTo(Proxy.Disabled)
+    }
+
+    @Test
+    fun `enableProxy() - applies given proxy and proxySetting is updated`() {
+        val proxy = Proxy(address = "1.2.3.4", port = 515)
+
+        subject.enableProxy(proxy)
+
+        assertThat(subject.proxySetting.value).isEqualTo(Proxy(address = "1.2.3.4", port = 515))
+    }
+
+    @Test
+    fun `disableProxy() - final state is disabled`() {
+        subject.disableProxy()
+
+        assertThat(subject.proxySetting.value).isEqualTo(Proxy.Disabled)
+    }
+}
