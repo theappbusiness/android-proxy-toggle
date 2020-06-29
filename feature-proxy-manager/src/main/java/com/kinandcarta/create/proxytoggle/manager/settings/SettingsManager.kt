92752ac868d8cc9129f8af3427debfe64816c65e
Fidel Montesino <fidel.montesino@gmail.com>
2020-06-29 18:24:58 +0100

    Remove the broken observer

--- feature-proxy-manager/src/main/java/com/kinandcarta/create/proxytoggle/manager/settings/SettingsManager.kt
+++ feature-proxy-manager/src/main/java/com/kinandcarta/create/proxytoggle/manager/settings/SettingsManager.kt
@@ -1,10 +1,7 @@
 package com.kinandcarta.create.proxytoggle.manager.settings
 
 import android.content.Context
-import android.database.ContentObserver
-import android.os.Handler
 import android.provider.Settings
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.kinandcarta.create.proxytoggle.manager.model.Proxy
@@ -21,28 +18,29 @@ class SettingsManager @Inject constructor(@ApplicationContext context: Context) 
         get() = _proxySetting
 
     init {
-        contentResolver.registerContentObserver(
-            Settings.Global.getUriFor(Settings.Global.HTTP_PROXY), true, ProxyContentObserver()
-        )
         updateProxyData()
     }
 
     fun enableProxy(proxy: Proxy) {
-        Settings.Global.putString(contentResolver, Settings.Global.HTTP_PROXY, proxy.toString())
+        Settings.Global.putString(
+            contentResolver,
+            Settings.Global.HTTP_PROXY,
+            proxy.toString()
+        )
+        updateProxyData()
     }
 
     fun disableProxy() {
-        Settings.Global.putString(contentResolver, Settings.Global.HTTP_PROXY, Proxy.Disabled.toString())
+        Settings.Global.putString(
+            contentResolver,
+            Settings.Global.HTTP_PROXY,
+            Proxy.Disabled.toString()
+        )
+        updateProxyData()
     }
 
     private fun updateProxyData() {
         val proxySetting = Settings.Global.getString(contentResolver, Settings.Global.HTTP_PROXY)
         _proxySetting.value = Proxy.from(proxySetting)
-    }
-
-    internal inner class ProxyContentObserver : ContentObserver(Handler()) {
-        override fun onChange(selfChange: Boolean) {
-            updateProxyData()
-        }
     }
 }
