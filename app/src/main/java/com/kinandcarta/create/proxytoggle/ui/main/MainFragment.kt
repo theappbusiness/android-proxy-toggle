c2e2fe2abfc6dfa5bc2159241ed8e8d0f01574ec
Fidel Montesino <fidel.montesino@gmail.com>
2020-06-29 15:33:55 +0100

    [Chore] Basic project setup (#1)
    * Added buildSrc plugin
    
    * Remove unnecessary files
    
    * Added detekt
    
    * Updated readme with a better structure
    
    * Remove .idea files
    
    * Enable newLine rule ¬¬
    
    * Added License info in Readme

--- app/src/main/java/com/kinandcarta/create/proxytoggle/ui/main/MainFragment.kt
+++ app/src/main/java/com/kinandcarta/create/proxytoggle/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.kinandcarta.create.proxytoggle.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.kinandcarta.create.proxytoggle.R
 
 class MainFragment : Fragment() {
@@ -16,15 +16,17 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        // TODO Use the ViewModel
     }
-
 }
