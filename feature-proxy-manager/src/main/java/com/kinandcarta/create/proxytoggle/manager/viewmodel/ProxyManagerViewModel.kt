70a8240b6c598a02a93606b9e45053dfad5b274c
Fidel Montesino <fidel.montesino@gmail.com>
2020-06-30 18:04:57 +0100

    Changed events to SingleLiveEvent

--- feature-proxy-manager/src/main/java/com/kinandcarta/create/proxytoggle/manager/viewmodel/ProxyManagerViewModel.kt
+++ feature-proxy-manager/src/main/java/com/kinandcarta/create/proxytoggle/manager/viewmodel/ProxyManagerViewModel.kt
@@ -1,12 +1,11 @@
 package com.kinandcarta.create.proxytoggle.manager.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.kinandcarta.create.proxytoggle.manager.android.DeviceSettingsManager
 import com.kinandcarta.create.proxytoggle.manager.android.ProxyValidator
-import com.kinandcarta.create.proxytoggle.manager.extensions.asLiveData
+import com.kinandcarta.create.proxytoggle.manager.extensions.SingleLiveEvent
 import com.kinandcarta.create.proxytoggle.manager.model.Proxy
 import com.kinandcarta.create.proxytoggle.manager.view.ProxyManagerEvent
 import com.kinandcarta.create.proxytoggle.manager.view.ProxyState
@@ -16,8 +15,7 @@ class ProxyManagerViewModel @ViewModelInject constructor(
     private val proxyValidator: ProxyValidator
 ) : ViewModel() {
 
-    private val _proxyEvent = MutableLiveData<ProxyManagerEvent>()
-    val proxyEvent = _proxyEvent.asLiveData()
+    val proxyEvent = SingleLiveEvent<ProxyManagerEvent>()
 
     val proxyState = Transformations.map(deviceSettingsManager.proxySetting) { proxy ->
         if (proxy.isEnabled) {
@@ -27,9 +25,9 @@ class ProxyManagerViewModel @ViewModelInject constructor(
 
     fun enableProxy(address: String, port: String) {
         if (!proxyValidator.isValidIP(address)) {
-            _proxyEvent.value = ProxyManagerEvent.InvalidAddress
+            proxyEvent.value = ProxyManagerEvent.InvalidAddress
         } else if (!proxyValidator.isValidPort(port)) {
-            _proxyEvent.value = ProxyManagerEvent.InvalidPort
+            proxyEvent.value = ProxyManagerEvent.InvalidPort
         } else {
             deviceSettingsManager.enableProxy(Proxy(address, port))
         }
