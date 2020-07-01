e69e817e89559fba8f48c48fc0021db3a9291350
Fidel Montesino <fidel.montesino@gmail.com>
2020-07-01 11:30:24 +0100

    Build small widget UI based on the current proxy state

--- feature-widget/src/main/java/com/kinandcarta/create/proxytoggle/widget/ToggleWidgetProvider.kt
+++ feature-widget/src/main/java/com/kinandcarta/create/proxytoggle/widget/ToggleWidgetProvider.kt
@@ -1,5 +1,44 @@
 package com.kinandcarta.create.proxytoggle.widget
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.view.View
+import android.widget.RemoteViews
+import androidx.lifecycle.Observer
+import com.kinandcarta.create.proxytoggle.lib.core.android.DeviceSettingsManager
+import com.kinandcarta.create.proxytoggle.lib.core.model.Proxy
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class ToggleWidgetProvider : AppWidgetProvider()
+@AndroidEntryPoint
+class ToggleWidgetProvider : AppWidgetProvider() {
+
+    @Inject
+    lateinit var deviceSettingsManager: DeviceSettingsManager
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        appWidgetIds.forEach { appWidgetId ->
+            // Instantiate the RemoteViews object for the app widget layout.
+            val remoteView = RemoteViews(context.packageName, R.layout.widget_toggle)
+
+            val proxy = deviceSettingsManager.proxySetting.value ?: Proxy.Disabled
+
+            if (proxy.isEnabled) {
+                remoteView.setTextViewText(R.id.button, "Disable")
+                remoteView.setViewVisibility(R.id.proxy, View.VISIBLE)
+                remoteView.setTextViewText(R.id.proxy, proxy.toString())
+            } else {
+                remoteView.setTextViewText(R.id.button, "Enable")
+                remoteView.setViewVisibility(R.id.proxy, View.GONE)
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteView)
+        }
+    }
+
+}
