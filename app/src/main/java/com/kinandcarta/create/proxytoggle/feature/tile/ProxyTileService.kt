736dfc98f140296a5fb5c2d3e7da0dfc4ccc539e
Fidel Montesino <fidel.montesino@gmail.com>
2020-07-02 11:22:04 +0100

    Add Quick Settings Tile to help toggle the proxy faster

---
+++ app/src/main/java/com/kinandcarta/create/proxytoggle/feature/tile/ProxyTileService.kt
@@ -0,0 +1,53 @@
+package com.kinandcarta.create.proxytoggle.feature.tile
+
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+import com.kinandcarta.create.proxytoggle.android.DeviceSettingsManager
+import com.kinandcarta.create.proxytoggle.model.Proxy
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@RequiresApi(Build.VERSION_CODES.N)
+@AndroidEntryPoint
+class ProxyTileService : TileService() {
+
+    @Inject
+    lateinit var deviceSettingsManager: DeviceSettingsManager
+
+    override fun onClick() {
+        toggleProxy()
+    }
+
+    override fun onStartListening() {
+        updateTile()
+    }
+
+    private fun toggleProxy() {
+        val proxy = deviceSettingsManager.proxySetting.value ?: Proxy.Disabled
+        if (proxy.isEnabled) {
+            deviceSettingsManager.disableProxy()
+        } else {
+            deviceSettingsManager.enableProxy(Proxy("192.168.1.215", "8888"))
+        }
+        updateTile()
+    }
+
+    private fun updateTile() {
+        val proxy = deviceSettingsManager.proxySetting.value ?: Proxy.Disabled
+        if (proxy.isEnabled) {
+            qsTile.apply {
+                label = proxy.toString()
+                state = Tile.STATE_ACTIVE
+                updateTile()
+            }
+        } else {
+            qsTile.apply {
+                label = "Proxy Disabled"
+                state = Tile.STATE_INACTIVE
+                updateTile()
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/kinandcarta/create/proxytoggle/feature/tile/ProxyTileServiceTest.kt
@@ -0,0 +1,102 @@
+package com.kinandcarta.create.proxytoggle.feature.tile
+
+import android.service.quicksettings.Tile
+import com.kinandcarta.create.proxytoggle.android.DeviceSettingsManager
+import com.kinandcarta.create.proxytoggle.model.Proxy
+import io.mockk.MockKAnnotations
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
+class ProxyTileServiceTest {
+
+    companion object {
+        private val PROXY = Proxy("192.168.1.215", "8888")
+    }
+
+    @RelaxedMockK
+    lateinit var mockDeviceSettingsManager: DeviceSettingsManager
+
+    @RelaxedMockK
+    lateinit var mockTile: Tile
+
+    private lateinit var subject: ProxyTileService
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        subject = spyk(ProxyTileService()).apply {
+            deviceSettingsManager = mockDeviceSettingsManager
+            every { qsTile } returns mockTile
+        }
+    }
+
+    @Test
+    fun `onClick() - GIVEN the proxy is disabled WHEN I click the tile THEN the proxy enables AND the tile is updated`() {
+        every { mockDeviceSettingsManager.proxySetting } returns mockk {
+            every { value } returns Proxy.Disabled andThen PROXY
+        }
+
+        subject.onClick()
+
+        verify { mockDeviceSettingsManager.enableProxy(PROXY) }
+        verifyTileIsEnabled(PROXY.toString())
+    }
+
+    @Test
+    fun `onClick() - GIVEN the proxy is enabled WHEN I click the tile THEN the proxy disables AND the tile is updated`() {
+        every { mockDeviceSettingsManager.proxySetting } returns mockk {
+            every { value } returns PROXY andThen Proxy.Disabled
+        }
+
+        subject.onClick()
+
+        verify { mockDeviceSettingsManager.disableProxy() }
+        verifyTileIsDisabled()
+    }
+
+    @Test
+    fun `onStartListening() - GIVEN the proxy is disabled THEN the tile updates to disabled state`() {
+        every { mockDeviceSettingsManager.proxySetting } returns mockk {
+            every { value } returns Proxy.Disabled
+        }
+
+        subject.onStartListening()
+
+        verifyTileIsDisabled()
+    }
+
+    @Test
+    fun `onStartListening() - GIVEN the proxy is enabled THEN the tile updates to enabled state`() {
+        every { mockDeviceSettingsManager.proxySetting } returns mockk {
+            every { value } returns PROXY
+        }
+
+        subject.onStartListening()
+
+        verifyTileIsEnabled(PROXY.toString())
+    }
+
+    private fun verifyTileIsEnabled(label: String) {
+        verify {
+            mockTile.label = label
+            mockTile.state = Tile.STATE_ACTIVE
+            mockTile.updateTile()
+        }
+        confirmVerified(mockTile)
+    }
+
+    private fun verifyTileIsDisabled() {
+        verify {
+            mockTile.label = "Proxy Disabled"
+            mockTile.state = Tile.STATE_INACTIVE
+            mockTile.updateTile()
+        }
+        confirmVerified(mockTile)
+    }
+}
