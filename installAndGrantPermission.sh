d2468e7977f39aee92329ada537a7dce7e198407
Fidel Montesino <fidel.montesino@gmail.com>
2020-07-06 10:43:42 +0100

    Minor changes pt 2

--- installAndGrantPermission.sh
+++ installAndGrantPermission.sh
@@ -4,7 +4,7 @@ if ! command -v adb &> /dev/null
 then
     echo "ADB needs to be in your PATH. The script will finish now."
     echo "Head over https://developer.android.com/studio/command-line/adb for more info about ADB."
-    echo "Here's a good step-by-step guide on how to setup ADB in your path: https://wiki.lineageos.org/adb_fastboot_guide.html"
+    echo "Here's a good step-by-step guide on how to setup ADB in your PATH: https://wiki.lineageos.org/adb_fastboot_guide.html"
     exit
 fi
 
