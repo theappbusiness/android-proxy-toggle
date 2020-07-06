141b9342d64771e6785aee8ea921e250ceaa210e
Fidel Montesino <fidel.montesino@gmail.com>
2020-07-06 09:28:56 +0100

    Add an apk and a small script for performing install and permission grant in one go

---
+++ installAndGrantPermission.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if ! command -v adb &> /dev/null
+then
+    echo "ADB needs to be in your path. The script will finish now."
+    echo "Head over https://developer.android.com/studio/command-line/adb for more info about ADB"
+    echo "Here's a good step-by-step guide on how to setup ADB in your path: https://wiki.lineageos.org/adb_fastboot_guide.html"
+    exit
+fi
+
+adb install -t -r proxy-toggle.apk
+adb shell pm grant com.kinandcarta.create.proxytoggle android.permission.WRITE_SECURE_SETTINGS
