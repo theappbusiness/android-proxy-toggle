b0d0b1f8d2926e910f75399982306976432b065e
Fidel Montesino <fidel.montesino@gmail.com>
2020-07-09 12:04:45 +0100

    Implement Widget UI (#20)
    * Fix launching new intent from tile
    
    * Add widget resources, dimens, etc
    
    * Add widget layout
    
    * Apply layout from Widget service
    
    * Small code tidy up
    
    * Update Unit Tests

--- app/src/test/java/com/kinandcarta/create/proxytoggle/feature/widget/ToggleWidgetProviderTest.kt
+++ app/src/test/java/com/kinandcarta/create/proxytoggle/feature/widget/ToggleWidgetProviderTest.kt
@@ -66,6 +66,7 @@ class ToggleWidgetProviderTest {
 
         subject = ToggleWidgetProvider().apply {
             deviceSettingsManager = mockDeviceSettingsManager
+            appSettings = mockAppSettings
         }
     }
 
